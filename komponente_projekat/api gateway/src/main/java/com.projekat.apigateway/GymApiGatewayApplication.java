065063c94cdcb20e7f422140a633717340b4f1df
AleksandarCumic <115494352+AleksandarCumic@users.noreply.github.com>
2024-01-17 13:55:50 +0100

    a

--- komponente_projekat/api gateway/src/main/java/com.projekat.apigateway/GymApiGatewayApplication.java
+++ komponente_projekat/api gateway/src/main/java/com.projekat.apigateway/GymApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package raf.gymapigateway;
+package com.projekat.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- komponente_projekat/eureka_service/src/main/java/raf/GymEurekaServiceApplication.java
+++ komponente_projekat/eureka_service/src/main/java/raf/GymEurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package raf.gymeurekaservice;
+package raf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
